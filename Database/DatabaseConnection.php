f83c888681c8595b6e9b0e20260aef43541b7bdc
Caio Victorino <caiodeveloper@hotmail.com>
2024-05-01 14:20:35 -0300

    base

--- Database/DatabaseConnection.php
+++ Database/DatabaseConnection.php
@@ -6,7 +6,7 @@ use PDO;
 class DatabaseConnection {
     private $dsn = 'mysql:dbname=caio_victorino;host=127.0.0.1';
     private $user = 'root';
-    private $password = '@Caio240894';
+    private $password = '!@Dev1994';
 
     public function connect(){
         $connect = new PDO($this->dsn, $this->user, $this->password);
